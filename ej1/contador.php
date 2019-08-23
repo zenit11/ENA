50a0d43ef40b8a5dd965e21b776c319d4663fe7d
zenit11 <54192541+zenit11@users.noreply.github.com>
2019-08-23 18:52:47 -0400

    Create contador.php

---
+++ ej1/contador.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Contador de visitas</title>
+</head>
+ 
+<body>
+<?php
+    function contadorvisitas()//  La función que ejecutara las visitas
+    {
+        $archivo = "contador.txt"; //el archivo donde se almacena las visitas
+        $f = fopen($archivo, "r"); //abrimos el fichero
+        if($f)
+        {
+            $contadorvisitas = fread($f, filesize($archivo)); //vemos el archivo a grabar
+            $contadorvisitas = $contadorvisitas + 1; // Le agregamos una visita mas al contador
+            fclose($f);
+        }
+        $f = fopen($archivo, "w+");
+        if($f)
+        {
+            fwrite($f, $contadorvisitas);
+            fclose($f);
+        }
+        return $contadorvisitas;
+    }
+	
+	//Mostramos las visitas acumuladas
+	echo "Registrados ";
+	echo contadorvisitas();//imprime las visitas
+	echo " Visitas totales.";
+ 
+?>
+</body>
+</html>
